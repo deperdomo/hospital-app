31f44ef59432cf2e93ff904e789edb9e0de0b048
Deivi <deivi54357326@gmail.com>
2025-01-20 13:53:06 +0100

    Create baner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from "./login/login.component";
 import { RegistroComponent } from "./registro/registro.component";
+import { BanerComponent } from "./baner/baner.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, RegistroComponent],
+  imports: [LoginComponent, RegistroComponent, BanerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
