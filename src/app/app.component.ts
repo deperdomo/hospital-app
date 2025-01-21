2ade6c1a29584cf784dce02b0e165d4cc6c5e36f
Deivi <deivi54357326@gmail.com>
2025-01-21 21:21:28 +0100

    Add InfoDoctor component and update app layout for improved doctor information display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { LoginComponent } from "./login/login.component";
 import { RegistroComponent } from "./registro/registro.component";
 import { BanerComponent } from "./baner/baner.component";
+import { InfoDoctorComponent } from "./info-doctor/info-doctor.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, RegistroComponent, BanerComponent],
+  imports: [LoginComponent, RegistroComponent, BanerComponent, InfoDoctorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/info-doctor/info-doctor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-doctor',
+  imports: [],
+  templateUrl: './info-doctor.component.html',
+  styleUrl: './info-doctor.component.css'
+})
+export class InfoDoctorComponent {
+
+}
