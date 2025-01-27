32d00dcf9feb177be467d455ee6bb7564b9f2982
Deivi <deivi54357326@gmail.com>
2025-01-27 19:18:06 +0100

    refactor: remove app-nearby-doctors-disabled component and add nearby-doctors-disabled component

---
+++ src/app/nearby-doctors-disabled/nearby-doctors-disabled.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nearby-doctors-disabled',
+  imports: [],
+  templateUrl: './nearby-doctors-disabled.component.html',
+  styleUrl: './nearby-doctors-disabled.component.css'
+})
+export class NearbyDoctorsDisabledComponent {
+
+}
