cbc4d31781cbe8f86b1ecd663baa5b8ad698d25a
Deivi <deivi54357326@gmail.com>
2025-01-27 19:41:23 +0100

    feat: add nearby-doctors-disabled component and integrate it into the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { BanerComponent } from "./baner/baner.component";
 import { InfoDoctorComponent } from "./info-doctor/info-doctor.component";
 import { NavBeforeLoginComponent } from "./nav-before-login/nav-before-login.component";
 import { NearbyDoctorsComponent } from "./nearby-doctors/nearby-doctors.component";
+import { NearbyDoctorsDisabledComponent } from "./nearby-doctors-disabled/nearby-doctors-disabled.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, RegistroComponent, BanerComponent, InfoDoctorComponent, NavBeforeLoginComponent, NearbyDoctorsComponent],
+  imports: [LoginComponent, RegistroComponent, BanerComponent, InfoDoctorComponent, NavBeforeLoginComponent, NearbyDoctorsComponent, NearbyDoctorsDisabledComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
