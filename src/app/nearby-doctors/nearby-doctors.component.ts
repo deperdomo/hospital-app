fe2ecbdcdbcc1cfbaaab1ae11153e54e89f261f2
Deivi <deivi54357326@gmail.com>
2025-01-27 12:43:39 +0100

    add component nearby-doctors

---
+++ src/app/nearby-doctors/nearby-doctors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nearby-doctors',
+  imports: [],
+  templateUrl: './nearby-doctors.component.html',
+  styleUrl: './nearby-doctors.component.css'
+})
+export class NearbyDoctorsComponent {
+
+}
