e8c370ba2c576024670ce3d8b2a3293258e2ced4
Deivi <deivi54357326@gmail.com>
2025-01-27 17:39:35 +0100

    add component app-nearby-doctors-disabled and update app component to include multiple instances

---
+++ src/app/app-nearby-doctors-disabled/app-nearby-doctors-disabled.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-nearby-doctors-disabled',
+  imports: [],
+  templateUrl: './app-nearby-doctors-disabled.component.html',
+  styleUrl: './app-nearby-doctors-disabled.component.css'
+})
+export class AppNearbyDoctorsDisabledComponent {
+
+}
