e96b36b9ece71fe8edd91ae84ada80ce6f1af74e
Deivi <deivi54357326@gmail.com>
2025-02-03 18:55:11 +0100

    Implementar lógica de navegación condicional y mejorar la estructura del componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
+
 import { BanerComponent } from "./baner/baner.component";
 import { InfoDoctorComponent } from "./info-doctor/info-doctor.component";
 import { NavBeforeLoginComponent } from "./nav-before-login/nav-before-login.component";
 import { NearbyDoctorsComponent } from "./nearby-doctors/nearby-doctors/nearby-doctors.component";
 import { NearbyDoctorsDisabledComponent } from "./nearby-doctors/nearby-doctors-disabled/nearby-doctors-disabled.component";
+
 
 @Component({
   selector: 'app-root',
@@ -19,5 +21,11 @@ export class AppComponent {
 
   cambiarEstado() {
     this.isNerbyDoctorsAvailable = true; // Cambiamos la variable a true
+  }
+
+  constructor(private router: Router) {}
+
+  esPaginaDeAutenticacion(): boolean {
+    return this.router.url.includes('/login') || this.router.url.includes('/registro');
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
+import { NgModule } from '@angular/core';
+
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { RegistroComponent } from './login/registro/registro.component';
-import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
-   
+
 ];
 
 @NgModule({
