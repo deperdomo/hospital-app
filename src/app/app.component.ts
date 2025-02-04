9f6d250be1c5a43a4a19c0e76a815bb41e4772e9
smoral1 <soledadmoral13@gmail.com>
2025-02-04 22:50:22 +0100

    Creado el left-nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { NearbyDoctorsComponent } from "./nearby-doctors/nearby-doctors/nearby-d
 import { NearbyDoctorsDisabledComponent } from "./nearby-doctors/nearby-doctors-disabled/nearby-doctors-disabled.component";
 import { FooterConmponent } from "./footer/footer.component";
 import { SecundaryNavComponent } from "./navs/secundary-nav/secundary-nav.component";
+import { LeftNavComponent } from "./navs/left-nav/left-nav.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [BanerComponent, InfoDoctorComponent, NavBeforeLoginComponent, NearbyDoctorsComponent, NearbyDoctorsDisabledComponent, RouterOutlet, FooterConmponent, SecundaryNavComponent],
+  imports: [BanerComponent, InfoDoctorComponent, NavBeforeLoginComponent, NearbyDoctorsComponent, NearbyDoctorsDisabledComponent, RouterOutlet, FooterConmponent, SecundaryNavComponent, LeftNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
