28b4d5d21ca2a9f3c52efd170c65149668ef5ea8
Deivi <deivi54357326@gmail.com>
2025-02-06 10:50:22 +0100

    Implementar componente IndexBeforeLogin con navegación y contenido inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
 
-import { InfoDoctorComponent } from "./sections/info-doctor/info-doctor.component";
-import { NavBeforeLoginComponent } from "./navs/nav-before-login/nav-before-login.component";
-import { NearbyDoctorsComponent } from "./nearby-doctors/nearby-doctors/nearby-doctors.component";
-import { NearbyDoctorsDisabledComponent } from "./nearby-doctors/nearby-doctors-disabled/nearby-doctors-disabled.component";
-import { FooterConmponent } from "./footer/footer.component";
-import { FindNavComponent } from "./navs/find-nav/find-nav.component";
-import { BanerComponent } from "./sections/baner/baner.component";
 
 
 @Component({
   selector: 'app-root',
 
-  imports: [BanerComponent, InfoDoctorComponent, NavBeforeLoginComponent, NearbyDoctorsComponent, NearbyDoctorsDisabledComponent, RouterOutlet, FooterConmponent, FindNavComponent],
+  imports: [ RouterOutlet],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  isNerbyDoctorsAvailable = false;
-
-  cambiarEstado() {
-    this.isNerbyDoctorsAvailable = true; // Cambiamos la variable a true
-  }
-
-  constructor(private router: Router) {}
-
-  esPaginaDeAutenticacion(): boolean {
-    return this.router.url.includes('/login') || this.router.url.includes('/registro');
-  }
 }
--- src/app/vistas/index-before-login/index-before-login.component.ts
+++ src/app/vistas/index-before-login/index-before-login.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { NavBeforeLoginComponent } from "../../navs/nav-before-login/nav-before-login.component";
+import { BanerComponent } from "../../sections/baner/baner.component";
+import { FindNavComponent } from "../../navs/find-nav/find-nav.component";
+import { InfoDoctorComponent } from "../../sections/info-doctor/info-doctor.component";
+import { NearbyDoctorsComponent } from "../../nearby-doctors/nearby-doctors/nearby-doctors.component";
+import { NearbyDoctorsDisabledComponent } from "../../nearby-doctors/nearby-doctors-disabled/nearby-doctors-disabled.component";
+import { FooterConmponent } from "../../footer/footer.component";
+
 
 @Component({
   selector: 'app-index-before-login',
-  imports: [],
+  imports: [NavBeforeLoginComponent, BanerComponent, FindNavComponent, InfoDoctorComponent, NearbyDoctorsComponent, NearbyDoctorsDisabledComponent, FooterConmponent],
   templateUrl: './index-before-login.component.html',
   styleUrl: './index-before-login.component.css'
 })
 export class IndexBeforeLoginComponent {
+  isNerbyDoctorsAvailable = false;
 
+  cambiarEstado() {
+    this.isNerbyDoctorsAvailable = true; // Cambiamos la variable a true
+  }
+
+  constructor(private router: Router) {}
+
+  esPaginaDeAutenticacion(): boolean {
+    return this.router.url.includes('/login') || this.router.url.includes('/registro');
+  }
 }
