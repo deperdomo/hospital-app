54200b5f0a88d76acdf208bb87d50027da20492a
Deivi <deivi54357326@gmail.com>
2025-02-06 10:45:10 +0100

    Agregar componente IndexBeforeLogin con plantilla inicial

---
+++ src/app/vistas/index-before-login/index-before-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-before-login',
+  imports: [],
+  templateUrl: './index-before-login.component.html',
+  styleUrl: './index-before-login.component.css'
+})
+export class IndexBeforeLoginComponent {
+
+}
