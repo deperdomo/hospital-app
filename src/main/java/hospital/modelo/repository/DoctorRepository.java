45c5923b8d1db4079e6388c7379689e63fb0398b
Deivi <deivi54357326@gmail.com>
2025-02-15 21:15:48 +0100

    Añadido metodo para buscar la disponibilidad por doctor

--- src/main/java/hospital/modelo/repository/DoctorRepository.java
+++ src/main/java/hospital/modelo/repository/DoctorRepository.java
@@ -1,9 +1,13 @@
 package hospital.modelo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import hospital.entidades.Doctor;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Integer>{
-
+	
+	List<Doctor> findByLocalidadContains(String localidad);
+	
 }
