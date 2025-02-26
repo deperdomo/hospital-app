de42e1780362f1abafbd6c1642f40c1b517e52e9
Deivi <deivi54357326@gmail.com>
2025-02-26 13:30:47 +0100

    Add doctor login functionality

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,21 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Usuario } from '../models/usuario';
+import { Doctor } from '../models/doctor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  public apiUrl = 'http://localhost:8090/usuario';
+  public apiUrl = 'http://localhost:8090';
 
   constructor(private http: HttpClient) {}
 
-  // Método para recuperar todo el usuario 
+  // Método para recuperar todo el usuario
   getUserByUsername(username: string): Observable<Usuario> {
-    console.log("Entrando al getUserByUsername");
-    
-    return this.http.get<Usuario>(`${this.apiUrl}/one/${username}`);
+    return this.http.get<Usuario>(`${this.apiUrl}/usuario/one/${username}`);
+  }
+
+  // Método para recuperar todo el doctor
+  getDoctorByUsername(username: string): Observable<Doctor> {
+    return this.http.get<Doctor>(`${this.apiUrl}/doctor/oneByUsername/${username}`);
   }
 
 }
